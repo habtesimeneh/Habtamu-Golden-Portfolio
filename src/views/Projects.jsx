fb088a0f77b0a7b8b2569fde7c9a26a26c69995f
Habte Simeneh <habtasimeneh30@gmail.com>
2026-07-12 10:48:15 -0700

    added

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -533,7 +533,7 @@ export default function Projects() {
                 {/* ============================================================ */}
                 <AnimatePresence>
                     {selectedProject && (
-                        <div className="fixed inset-0 z-9999 flex items-center justify-center p-6 bg-black/60 dark:bg-black/90 backdrop-blur-md">
+                        <div className="fixed inset-0  top-2 z-9999 flex items-center justify-center p-6 bg-black/60 dark:bg-black/90 backdrop-blur-md">
                             {/* Overlay background trigger close */}
                             <div className="absolute inset-0" onClick={() => setSelectedProject(null)} />
 
